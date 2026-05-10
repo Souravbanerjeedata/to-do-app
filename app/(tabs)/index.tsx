07044c58c73e54ef23de606a4b5389d5d4b7c31d
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-05-10 12:44:50 +0530

    created a custom hook 'useTheme' to change the theme to light or dark

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import useTheme from "@/hooks/useTheme";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const { toggleDarkMode } = useTheme();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>toggle the mode</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function settings() {
   return (
@@ -8,5 +8,3 @@ export default function settings() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
