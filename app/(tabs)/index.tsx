010bdcd22241c5dc58b8404fe4c226961fb3ea25
Sourav Banerjee <sourav.banerjeejobs@gmail.com>
2026-05-10 14:43:11 +0530

    setting up the backend with Convex

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,10 @@
 import useTheme from "@/hooks/useTheme";
+
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const { toggleDarkMode } = useTheme();
+
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={toggleDarkMode}>
